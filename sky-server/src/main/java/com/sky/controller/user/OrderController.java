85a971e5af386bd279862d547e76f12a28569c5c
ziyan <zhouziyanzjdx@163.com>
2025-10-02 23:05:46 +0200

    address CRUD, order submit and order payment emulation function finished.

---
+++ sky-server/src/main/java/com/sky/controller/user/OrderController.java
@@ -0,0 +1,62 @@
+package com.sky.controller.user;
+
+
+import com.sky.dto.OrdersPaymentDTO;
+import com.sky.dto.OrdersSubmitDTO;
+import com.sky.result.Result;
+import com.sky.service.OrderService;
+import com.sky.vo.OrderOverViewVO;
+import com.sky.vo.OrderPaymentVO;
+import com.sky.vo.OrderSubmitVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@Api(tags = "order relevant api")
+@RestController("userOrderController")
+@RequestMapping("/user/order")
+@Slf4j
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    /**
+     * user submit order
+     * @param ordersSubmitDTO
+     * @return
+     */
+    @ApiOperation("order submit")
+    @PostMapping("/submit")
+    public Result<OrderSubmitVO> submitOrder(@RequestBody OrdersSubmitDTO ordersSubmitDTO){
+
+        log.info("zzy_log order info: {}",ordersSubmitDTO);
+
+        OrderSubmitVO orderSubmitVO = orderService.submit(ordersSubmitDTO);
+
+        return Result.success(orderSubmitVO);
+    }
+
+
+    /**
+     * order payment
+     *
+     * @param ordersPaymentDTO
+     * @return
+     */
+    @PutMapping("/payment")
+    @ApiOperation("order payment")
+    public Result<OrderPaymentVO> payment(@RequestBody OrdersPaymentDTO ordersPaymentDTO) throws Exception {
+        log.info("zzy_log order payment：{}", ordersPaymentDTO);
+        OrderPaymentVO orderPaymentVO = orderService.payment(ordersPaymentDTO);
+        log.info("generate pre-payment invoice：{}", orderPaymentVO);
+
+//        emulate payment success, modify db order status
+        orderService.paySuccess(ordersPaymentDTO.getOrderNumber());
+        log.info("zzy_log emulate payment success: {}",ordersPaymentDTO.getOrderNumber());
+
+        return Result.success(orderPaymentVO);
+    }
+}
---
+++ sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
@@ -0,0 +1,169 @@
+package com.sky.service.impl;
+
+import com.alibaba.fastjson.JSONObject;
+import com.sky.constant.MessageConstant;
+import com.sky.context.BaseContext;
+import com.sky.dto.OrdersPaymentDTO;
+import com.sky.dto.OrdersSubmitDTO;
+import com.sky.entity.*;
+import com.sky.exception.AddressBookBusinessException;
+import com.sky.exception.OrderBusinessException;
+import com.sky.exception.ShoppingCartBusinessException;
+import com.sky.mapper.*;
+import com.sky.service.OrderService;
+import com.sky.utils.WeChatPayUtil;
+import com.sky.vo.OrderPaymentVO;
+import com.sky.vo.OrderSubmitVO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Slf4j
+public class OrderServiceImpl implements OrderService {
+
+    @Autowired
+    private OrderMapper orderMapper;
+
+    @Autowired
+    private AddressBookMapper addressBookMapper;
+
+    @Autowired
+    private ShoppingCartMapper shoppingCartMapper;
+
+    @Autowired
+    private OrderDetailMapper orderDetailMapper;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private WeChatPayUtil weChatPayUtil;
+
+
+    /**
+     * user submit order
+     * @param ordersSubmitDTO
+     */
+    @Override
+    @Transactional(rollbackFor = RuntimeException.class)
+    public OrderSubmitVO submit(OrdersSubmitDTO ordersSubmitDTO) {
+
+//        1. exception handler, although frontend will verify it
+//        but to make the project robust, we can verify again for example for api test case
+        AddressBook addressBook = addressBookMapper.getById(ordersSubmitDTO.getAddressBookId());
+        if (addressBook == null){
+            throw new AddressBookBusinessException((MessageConstant.ADDRESS_BOOK_IS_NULL));
+        }
+
+        Long userId = BaseContext.getCurrentId();
+        ShoppingCart shoppingCart = new ShoppingCart();
+        shoppingCart.setUserId(userId);
+
+//        2. check user shopping cart info
+        List<ShoppingCart> shoppingCartList = shoppingCartMapper.getByUserId(userId);
+        if (shoppingCartList == null && shoppingCartList.size() > 0){
+            throw new ShoppingCartBusinessException(MessageConstant.SHOPPING_CART_IS_NULL);
+        }
+
+//        3. create order data
+        Orders order = new Orders();
+        BeanUtils.copyProperties(ordersSubmitDTO, order);
+        order.setPhone(addressBook.getPhone());
+        order.setAddress(addressBook.getDetail());
+        order.setConsignee(addressBook.getConsignee());
+        order.setNumber(String.valueOf(System.currentTimeMillis()));
+        order.setUserId(userId);
+        order.setStatus(Orders.PENDING_PAYMENT);
+        order.setPayStatus(Orders.UN_PAID);
+        order.setOrderTime(LocalDateTime.now());
+
+//        4. insert order into db, use generatedKey, later we need id for OrderDetail table
+        orderMapper.insert(order);
+
+//        5. insert order details(dishes, setmeals in the order, 1 order : m order_details)
+        List<OrderDetail> orderDetailList = new ArrayList<>();
+        for (ShoppingCart cart : shoppingCartList) {
+            OrderDetail orderDetail = new OrderDetail();
+            BeanUtils.copyProperties(cart,orderDetail);
+            orderDetail.setOrderId(order.getId());
+            orderDetailList.add(orderDetail);
+        }
+        orderDetailMapper.batchInsert(orderDetailList);
+
+//        6. clear shopping cart
+        shoppingCartMapper.deleteByUserId(userId);
+
+//        7. return submitted order info to frontend
+        return OrderSubmitVO.builder()
+                .id(order.getId())
+                .orderNumber(order.getNumber())
+                .orderAmount(order.getAmount())
+                .orderTime(order.getOrderTime())
+                .build();
+
+    }
+
+
+    /**
+     * 订单支付
+     *
+     * @param ordersPaymentDTO
+     * @return
+     */
+    public OrderPaymentVO payment(OrdersPaymentDTO ordersPaymentDTO) throws Exception {
+        // 当前登录用户id
+        Long userId = BaseContext.getCurrentId();
+        User user = userMapper.getById(userId);
+
+//        warning: access with 3rd party api need to avoid
+        //调用微信支付接口，生成预支付交易单
+        /*JSONObject jsonObject = weChatPayUtil.pay(
+                ordersPaymentDTO.getOrderNumber(), //商户订单号
+                new BigDecimal(0.01), //支付金额，单位 元
+                "sky_takeout order", //商品描述
+                user.getOpenid() //微信用户的openid
+        );*/
+        JSONObject jsonObject = new JSONObject();
+//        jsonObject.put("code","ORDERPAID");
+
+
+
+        if (jsonObject.getString("code") != null && jsonObject.getString("code").equals("ORDERPAID")) {
+            throw new OrderBusinessException("该订单已支付");
+        }
+
+        OrderPaymentVO vo = jsonObject.toJavaObject(OrderPaymentVO.class);
+        vo.setPackageStr(jsonObject.getString("package"));
+
+        return vo;
+    }
+
+    /**
+     * 支付成功，修改订单状态
+     *
+     * @param outTradeNo
+     */
+    public void paySuccess(String outTradeNo) {
+
+        // 根据订单号查询订单
+        Orders ordersDB = orderMapper.getByNumber(outTradeNo);
+
+        // 根据订单id更新订单的状态、支付方式、支付状态、结账时间
+        Orders orders = Orders.builder()
+                .id(ordersDB.getId())
+                .status(Orders.TO_BE_CONFIRMED)
+                .payStatus(Orders.PAID)
+                .checkoutTime(LocalDateTime.now())
+                .build();
+
+        orderMapper.update(orders);
+    }
+}
