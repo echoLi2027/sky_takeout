b91113708879129fd9cf742bad255a1736cb44d9
ziyan <zhouziyanzjdx@163.com>
2025-10-12 21:58:31 +0200

    work space panel partially implement.

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -13,6 +13,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface DishMapper {
@@ -53,4 +54,11 @@ public interface DishMapper {
      */
     @Select("select * from dish where category_id = #{categoryID} and status = 1")
     List<Dish> selectByCategoryId(Long categoryId);
+
+    /**
+     * 根据条件统计菜品数量
+     * @param map
+     * @return
+     */
+    Integer countByMap(Map map);
 }
